6d905a81bfc2834d013e8862a4e38d3960209695
vince <vincecole6@hotmail.co.uk>
2024-04-09 00:51:58 +0100

    Improve the test slightly

--- api/tests/Feature/Auth/ApiApps/ApiAppTest.php
+++ api/tests/Feature/Auth/ApiApps/ApiAppTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\App as APIApp;
 
+use function PHPUnit\Framework\assertCount;
+
 class ApiAppTest extends TestCase
 {
     use RefreshDatabase;
@@ -22,6 +24,9 @@ class ApiAppTest extends TestCase
     /**
      * It generates a new app and assigns it a unique access token
      * 
+     * Could be improved by checking for more than 'not null', eg
+     * string length, data type, data structure etc.
+     * 
      * @test
      * @group ApiApp
      */
@@ -31,6 +36,8 @@ class ApiAppTest extends TestCase
         $appKey = $apiApp->app_access_id;
         $token = $apiApp->accessToken->first()->secret_token;
 
+        $this->assertTrue(APIApp::count() === 1);
+
         $this->assertNotNull($token);
         $this->assertNotNull($appKey);
      }
@@ -38,13 +45,18 @@ class ApiAppTest extends TestCase
      /**
      * It generates a new app and assigns it a unique access token using console command
      * 
+     * Could be improved by checking for more than 'not null', eg
+     * string length, data type, data structure etc.
+     * 
      * @test
      * @group ApiApp
      */
      public function it_can_generate_a_new_app_token_pair_via_console_command(){
         $this->artisan('app:generate', ['name'=>'Test Application']);
         $newApplication = APIApp::with('accessToken')->first();
-        
+
+        $this->assertTrue(APIApp::count() === 1);
+
         $this->assertTrue($newApplication->app_name === 'Test Application');
         $this->assertNotNull($newApplication->app_access_id);
         $this->assertNotNull($newApplication->accessToken->first()->secret_token);
