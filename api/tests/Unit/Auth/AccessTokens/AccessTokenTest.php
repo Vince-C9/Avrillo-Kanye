8ba98bc016a9a69fb2e2bd21d17b067e9ce56843
vince <vincecole6@hotmail.co.uk>
2024-04-08 22:18:53 +0100

    Very simple tests to make sure the settings work.

---
+++ api/tests/Unit/Auth/AccessTokens/AccessTokenTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit\Auth\AccessTokens;
+
+use App\Models\AccessToken;
+use PHPUnit\Framework\TestCase;
+
+class AccessTokenTest extends TestCase
+{
+    
+
+
+    /**
+     * A basic unit test example.
+     * @group AccessTokens
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    
+
+}
