7bf0b5e2199d13f52646a47c14c32b2bb33b5ea6
vince <vincecole6@hotmail.co.uk>
2024-04-09 15:41:56 +0100

    Remove comma, use correct array format throughout tests.  Delete test test, un-needed

--- api/tests/Feature/Auth/AccessTokens/AccessTokenTest.php
+++ api/tests/Feature/Auth/AccessTokens/AccessTokenTest.php
@@ -13,30 +13,19 @@ class AccessTokenTest extends TestCase
 {
     use RefreshDatabase;
     /**
-     * A basic feature test example.
-     * @group AccessTokens
-     */
-
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
-    /**
      * Returns existing AT if it exists and is valid
      *
      * @test
      * @group AccessTokens
      */
     public function it_returns_an_existing_valid_token_if_one_exists(){
+        
         $app = App::factory()->create(['id' => 1]);
+        
         AccessToken::factory()->create([
-            'secret_token','totallysecuresecrettoken123', 
-            'app_id'=>1
+            'access_token'=>'totallysecuresecrettoken123', 
+            'app_id'=>$app->id
         ]);
-
         $token = AccessToken::GenerateNewAccessToken($app);
         $this->assertTrue($token === 'totallysecuresecrettoken123');
 
@@ -52,7 +41,7 @@ class AccessTokenTest extends TestCase
         $app = App::factory()->create(['id' => 1]);
         $token = AccessToken::GenerateNewAccessToken($app);
 
-        $this->assertNotEmpty($app->accessToken->first()->secret_token);
+        $this->assertNotEmpty($app->accessToken->first()->access_token);
     }
 
     /**
@@ -64,7 +53,7 @@ class AccessTokenTest extends TestCase
     public function it_creates_a_new_token_if_all_tokens_are_invalid(){
         $app = App::factory()->create(['id' => 1]);
         AccessToken::factory()->create([
-            'secret_token','totallysecuresecrettoken123', 
+            'access_token' => 'totallysecuresecrettoken123', 
             'expiration_date'=>Carbon::now()->subDays(1),
             'app_id'=>1
         ]);
