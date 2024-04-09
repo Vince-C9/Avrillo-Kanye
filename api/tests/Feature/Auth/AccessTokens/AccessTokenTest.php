64ca92804f4379dfd932d8db1c4b34fa482cbad5
vince <vincecole6@hotmail.co.uk>
2024-04-09 15:05:24 +0100

    tests for access token generation

--- api/tests/Feature/Auth/AccessTokens/AccessTokenTest.php
+++ api/tests/Feature/Auth/AccessTokens/AccessTokenTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\AccessToken;
+use App\Models\App;
+use Illuminate\Support\Carbon;
 
 class AccessTokenTest extends TestCase
 {
@@ -23,15 +25,52 @@ class AccessTokenTest extends TestCase
     }
 
     /**
-     * An access token can be generated
-     * 
+     * Returns existing AT if it exists and is valid
+     *
      * @test
      * @group AccessTokens
      */
+    public function it_returns_an_existing_valid_token_if_one_exists(){
+        $app = App::factory()->create(['id' => 1]);
+        AccessToken::factory()->create([
+            'secret_token','totallysecuresecrettoken123', 
+            'app_id'=>1
+        ]);
 
-     public function it_can_generate_a_unique_access_token(){
-        $token = AccessToken::factory()->create(['app_id'=>1]);
+        $token = AccessToken::GenerateNewAccessToken($app);
+        $this->assertTrue($token === 'totallysecuresecrettoken123');
 
-        $this->assertTrue(!is_null($token));
-     }
+    }
+
+    /**
+     * Creates a new token if it doesn't exist 
+     *
+     * @test
+     * @group AccessTokens
+     */
+    public function it_creates_a_new_token_if_one_doesnt_exist(){
+        $app = App::factory()->create(['id' => 1]);
+        $token = AccessToken::GenerateNewAccessToken($app);
+
+        $this->assertNotEmpty($app->accessToken->first()->secret_token);
+    }
+
+    /**
+     * Creates a new token if it only finds invalid ones 
+     *
+     * @test
+     * @group AccessTokens
+     */
+    public function it_creates_a_new_token_if_all_tokens_are_invalid(){
+        $app = App::factory()->create(['id' => 1]);
+        AccessToken::factory()->create([
+            'secret_token','totallysecuresecrettoken123', 
+            'expiration_date'=>Carbon::now()->subDays(1),
+            'app_id'=>1
+        ]);
+
+        $token = AccessToken::GenerateNewAccessToken($app);
+        $this->assertNotEmpty($token);
+        $this->assertTrue($token !== 'totallysecuresecrettoken123');
+    }
 }
