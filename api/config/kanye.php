c726d3c6da3b6dddc1239f0b26901804807b4626
vince <vincecole6@hotmail.co.uk>
2024-04-10 15:38:15 +0100

    Setup quote fetching test

---
+++ api/config/kanye.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'api_endpoint'=>'https://api.kanye.rest/'
+];
--- api/tests/Feature/Quote/QuotesTest.php
+++ api/tests/Feature/Quote/QuotesTest.php
@@ -2,21 +2,55 @@
 
 namespace Tests\Feature\Quote;
 
+use App\Models\App as APIApp;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Http;
+use App\Models\AccessToken;
+use Illuminate\Support\Facades\Config;
 
 class QuotesTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private $token;
+    private $apiApp;
+
     /**
-     * Undocumented function
+     * Each time a test runs, set up at least one app and access token.
+     *
+     * @return void
+     */
+    public function setUp(): void{
+
+        parent::setUp();
+        $this->apiApp = APIApp::factory()->create();
+        $this->token = Accesstoken::generateNewAccessToken($this->apiApp);
+        
+    }
+
+    /**
+     * It calls the endpoint to generate quotes and returns 5.
      *
      * @test
      * @group Quotes
      */
     public function it_generates_kanye_quotes_from_the_kanye_api(){
+        Http::fake([
+            Config::get('kanye.api_endpoint') => Http::response([
+                ['quote'], 200, ['Headers'],
+            ])
+        ]);
 
+        $response = $this->get(route('quote.get'), ['Authorization' => "Bearer ".$this->token]);
+
+        //It sent 5 requests for quotes
+        Http::assertSentCount(5);
+        $response->assertOk();
+        $response->assertSee('quotes');
+        $response->assertJsonIsArray('quotes');
     }
 
-    
+
 }
