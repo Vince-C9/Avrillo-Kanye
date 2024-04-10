c78d3e305b11e2406392ad31c940234f10f9f23d
vince <vincecole6@hotmail.co.uk>
2024-04-10 02:07:22 +0100

    Add quotes test skeleton

---
+++ api/tests/Feature/Quote/QuotesTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Quote;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class QuotesTest extends TestCase
+{
+    /**
+     * Undocumented function
+     *
+     * @test
+     * @group Quotes
+     */
+    public function it_generates_kanye_quotes_from_the_kanye_api(){
+
+    }
+
+    
+}
