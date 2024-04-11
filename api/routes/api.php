79bc7e5b44659e4f14a723b32893f19140781e8a
vince <vincecole6@hotmail.co.uk>
2024-04-11 20:37:14 +0100

    Implement refresh quotes endpoint

--- api/routes/api.php
+++ api/routes/api.php
@@ -25,6 +25,11 @@ Route::middleware(AccessTokenAuthentication::class)->get('/health-check', functi
 //Auth routes
 Route::prefix('quote.')->name('quote.')->middleware(AccessTokenAuthentication::class)->group(function(){
     Route::get('quote', [QuoteController::class, 'index'])->name('get');
+    /**
+     * Couldn't settle on whether DESTROY fitted here.  
+     * I've gone for refresh as it affords a little more readability I feel.
+     */
+    Route::get('quote/refresh', [QuoteController::class, 'refresh'])->name('refresh');
 });
 
 
