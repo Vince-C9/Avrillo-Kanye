810506e719f68608674a6d9131d91994b5570d2d
vince <vincecole6@hotmail.co.uk>
2024-04-11 02:35:06 +0100

    Quote controller

--- api/app/Http/Controllers/QuoteController.php
+++ api/app/Http/Controllers/QuoteController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\QuoteService;
 use Illuminate\Http\Request;
+use Throwable;
 
 class QuoteController extends Controller
 {
-    public function index(){
-        
+    //Protected by middleware - should only get here if AT is present.
+    public function index(Request $request) {
+        try {
+            $quoteService = new QuoteService;
+            $quotes = $quoteService->getQuote($request, 5);
+            return response()->json(['quotes'=>$quotes]);
+        } catch (Throwable $t) {
+            report($t->getMessage());
+            throw $t;
+        }
     }
+
+    
 }
